4391395f4af9ed7a80807097112fd6e53ad31b60
msands <michaelesands@gmail.com>
2023-01-05 22:05:09 -0500

    industry standard

---
+++ lib/holdings/fixedincome/insurance/gic.py
@@ -0,0 +1,21 @@
+
+
+class GIC:
+    ''' A base class for instantiating objects of various flavors of GIC's based on a holdings template json file '''
+
+    def __init__(self) -> None:
+        pass
+    
+
+
+
+class SAGIC:
+
+    def __init__(self) -> None:
+        pass
+
+
+class TRADGIC:
+
+    def __init__(self) -> None:
+        pass
